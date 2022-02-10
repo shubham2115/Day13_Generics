9064e0cc3fc2e0e0dbd868d578f5ae8d29159d19
Shubham <s.shrikhande1@gmail.com>
2022-02-11 02:29:35 +0530

    [Shubham]:Added int maximum UC_1

---
+++ MaxNumber/MaxNUmberCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_13_Generics
+{
+    public class MaxNUmberCheck
+    {
+
+        public int IntMax(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+            {
+                return secondNum;
+            }
+            return thirdNum;
+
+        }
+    }
+}
